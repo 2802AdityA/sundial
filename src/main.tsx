9a583b9cebc70e62d097df640387345c3ed85999
Aditya Singla <adityasingla.2802@gmail.com>
2024-07-20 20:13:51 +0530

    Wrapped the app component with context component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { KpiProvider, MetricsProvider, SegmentsProvider } from './context'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <KpiProvider>
+      <MetricsProvider>
+        <SegmentsProvider>
+          <App />
+        </SegmentsProvider>
+      </MetricsProvider>
+    </KpiProvider>
   </React.StrictMode>,
 )
