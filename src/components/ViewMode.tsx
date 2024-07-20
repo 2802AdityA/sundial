544181272ca97ccb00c65195b4d6b44c0af98545
Aditya Singla <adityasingla.2802@gmail.com>
2024-07-20 20:12:19 +0530

    Add view mode

---
+++ src/components/ViewMode.tsx
@@ -0,0 +1,29 @@
+import * as Highcharts from 'highcharts';
+import { HighchartsReact } from 'highcharts-react-official';
+import { options } from '../config'
+import { useEffect, useRef } from 'react';
+import { Kpi } from '../interface';
+
+export const ViewMode = ({ kpi }: { kpi: Kpi }) => {
+
+    const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
+    console.log(kpi);
+    // useEffect(() => {
+
+    // })
+    return (
+        <div className='flex flex-col'>
+
+            <div className='flex justify-end'>
+
+                <div className='w-4/5 h-1/4'>
+                    <HighchartsReact
+                        highcharts={Highcharts}
+                        options={options}
+                        ref={chartComponentRef}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
